eb797bb6e0232be1504446dea6116353f304865a
Rahul Lingwal <rahullingwal200@gmail.com>
2026-06-21 22:36:37 +0530

    Add Cycle Sort

--- src/Java03_Sorting/Cycle_Sort/CycleSort.java
+++ src/Java03_Sorting/Cycle_Sort/CycleSort.java
@@ -1,7 +1,33 @@
 package Java03_Sorting.Cycle_Sort;
 
+import java.util.Arrays;
+
 public class CycleSort {
     public static void main(String[] args) {
+        int[] arr = {5, 9, 1, 2, 7, 3, 4, 6, 8, 10};
+        System.out.printf("Unsorted array = %s \n", Arrays.toString(arr));
 
+        cycleSort(arr);
+
+        System.out.printf("Sorted array = %s", Arrays.toString(arr));
+    }
+
+    static void cycleSort(int[] arr) {
+        int i = 0;
+
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int correctIndex) {
+        int temp = arr[correctIndex];
+        arr[correctIndex] = arr[i];
+        arr[i] = temp;
     }
 }
