0d3725eee637a124cc6767dae55f175b129b976a
Rahul Lingwal <rahullingwal200@gmail.com>
2026-07-19 15:37:47 +0530

    practice: solve maze paths includin diagonal using recursion

---
+++ src/Java04_Recursion/Backtracking/MazeWithDiagonal.java
@@ -0,0 +1,34 @@
+package Java04_Recursion.Backtracking;
+
+import java.util.ArrayList;
+
+public class MazeWithDiagonal {
+    public static void main(String[] args) {
+        ArrayList<String> ans = getPathsDiagonal("", 3, 3);
+        System.out.println(ans);
+    }
+
+    static ArrayList<String> getPathsDiagonal(String processed, int row, int col) {
+        if (row == 1 && col == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (row > 1) {
+            list.addAll(getPathsDiagonal(processed + 'D', row - 1, col));
+        }
+
+        if (row > 1 && col > 1) {
+            list.addAll(getPathsDiagonal(processed + 'd', row - 1, col - 1));
+        }
+
+        if (col > 1) {
+            list.addAll(getPathsDiagonal(processed + 'R', row, col - 1));
+        }
+
+        return list;
+    }
+}
