936971fe68ac87c1cdf17d8cef9341dbcf81a922
Rahul Lingwal <rahullingwal200@gmail.com>
2026-07-21 22:52:47 +0530

    practice:  solve N-Knights problem with backtracking

---
+++ src/Java04_Recursion/Backtracking/NKnights.java
@@ -0,0 +1,84 @@
+package Java04_Recursion.Backtracking;
+
+public class NKnights {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        placeKnights(board, 0, 0, 4);
+    }
+
+    static void placeKnights(boolean[][] board, int row, int col, int knights) {
+        if (knights == 0) {
+            display(board);
+            System.out.println();
+            return;
+        }
+
+        //  Reached the end of the board
+        if (row == board.length - 1 && col == board.length) {
+            return;
+        }
+
+        // End of current row, continue from the first column of the next row
+        if (col == board.length) {
+            placeKnights(board, row + 1, 0, knights);
+            return;
+        }
+
+        if (isSafe(board, row, col)) {
+            board[row][col] = true;
+            placeKnights(board, row, col + 1, knights - 1);
+            board[row][col] = false;
+        }
+
+        placeKnights(board, row, col + 1, knights);
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        if (isValid(board, row - 2, col - 1)) {
+            if (board[row - 2][col - 1]) {
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 1, col - 2)) {
+            if (board[row - 1][col - 2]) {
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 2, col + 1)) {
+            if (board[row - 2][col + 1]) {
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 1, col + 2)) {
+            if (board[row - 1][col + 2]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isValid(boolean[][] board, int row, int col) {
+        if (row >= 0 && row < board.length && col >= 0 && col < board.length) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void display(boolean[][] board) {
+        for (boolean[] row : board) {
+            for (boolean element : row) {
+                if (element) {
+                    System.out.print("K ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
