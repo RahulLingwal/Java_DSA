37ee5f7036d3016146d7ca7a3c2a955e16984f1d
Rahul Lingwal <rahullingwal200@gmail.com>
2026-08-16 15:58:22 +0530

    feat: add insert method with next and pos to track position.

--- src/Java08_Linked_List/Questions/ListNode.java
+++ src/Java08_Linked_List/Questions/ListNode.java
@@ -4,23 +4,30 @@ public class ListNode {
     ListNode head;
     ListNode next;
     int val;
+    int pos;
 
     ListNode() {
+        this.pos = -1;
     }
 
     ListNode(int val) {
         this.val = val;
+        this.pos = -1;
     }
 
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+        this.pos = -1;
     }
 
     public void insert(int val) {
         ListNode node = new ListNode(val, null);
+        node.pos = pos + 1;
+
         if (head == null) {
             head = node;
+            pos++;
             return;
         }
 
@@ -29,6 +36,25 @@ public class ListNode {
             cur = cur.next;
         }
         cur.next = node;
+        pos++;
+    }
+
+    public void insert(int val, ListNode next) {
+        ListNode node = new ListNode(val, next);
+        node.pos = pos + 1;
+
+        if (head == null) {
+            head = node;
+            pos++;
+            return;
+        }
+
+        ListNode cur = head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = node;
+        pos++;
     }
 
     public void display() {
