07ddc06c1fc8f5c0207ed5b13bb8b21898b9a97b
Rahul Lingwal <rahullingwal200@gmail.com>
2026-08-28 20:28:46 +0530

    leetcode: add 20. Valid Parentheses solution

---
+++ src/Java09_Stack/Questions/ValidParentheses.java
@@ -0,0 +1,51 @@
+package Java09_Stack.Questions;
+
+// https://leetcode.com/problems/valid-parentheses/
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter parentheses = ");
+        String str = sc.next();
+
+        if (ParenthesesSolution.isValid(str)) {
+            System.out.println("Valid Parentheses.");
+        } else {
+            System.out.println("Not valid Parentheses.");
+        }
+    }
+}
+
+class ParenthesesSolution {
+    public static boolean isValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (ch == ')') {
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                }
+
+                if (ch == '}') {
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                }
+
+                if (ch == ']') {
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
